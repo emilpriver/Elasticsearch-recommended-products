63834cf414645eae2afaeb75bb092e5e2abed65e
Emil Privér <emil@rivercode.se>
2022-01-09 18:44:41 +0100

    dev

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,3 +1,4 @@
+use elasticsearch::Elasticsearch;
 use serde_json::json;
 use worker::*;
 
@@ -17,17 +18,11 @@ fn log_request(req: &Request) {
 pub async fn main(req: Request, env: Env) -> Result<Response> {
     log_request(&req);
 
-    // Optionally, get more helpful error messages written to the console in the case of a panic.
     utils::set_panic_hook();
 
-    // Optionally, use the Router to handle matching endpoints, use ":name" placeholders, or "*name"
-    // catch-alls to match on specific patterns. Alternatively, use `Router::with_data(D)` to
-    // provide arbitrary data that will be accessible in each route via the `ctx.data()` method.
     let router = Router::new();
+    let elastic_client = Elasticsearch::default();
 
-    // Add as many routes as your Worker needs! Each route will get a `Request` for handling HTTP
-    // functionality and a `RouteContext` which you can use to  and get route parameters and
-    // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
     router
         .get("/", |_, _| Response::ok("Hello from Workers!"))
         .post_async("/form/:field", |mut req, ctx| async move {
@@ -44,6 +39,12 @@ pub async fn main(req: Request, env: Env) -> Result<Response> {
                 }
             }
 
+            Response::error("Bad Request", 400)
+        })
+        .post_async("/order", |mut req, ctx| async move {
+            let order = ctx.param("order");
+            let products = ctx.param("products");
+        
             Response::error("Bad Request", 400)
         })
         .get("/worker-version", |_, ctx| {
