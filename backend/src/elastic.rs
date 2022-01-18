9efc0fb98c7095f8027082c65cd4d7dd6477d4a8
Emil Privér <emil@rivercode.se>
2022-01-18 15:27:33 +0100

    Worked on check if index exists before bulk_add command send data to elastic

--- backend/src/elastic.rs
+++ backend/src/elastic.rs
@@ -4,6 +4,8 @@
  * This class is made as Elasticsearch official client is not finished yet and don't seem to work with Cloudflare WASM
  */
 
+use std::collections::HashMap;
+
 pub struct Elasticsearch {
   uri: String,
 }
@@ -15,7 +17,28 @@ impl Elasticsearch {
     Elasticsearch { uri }
   }
 
-  async fn check_if_index_exists(self, index: String) -> bool {
+  async fn create_index(self, index: String) -> Result<String, String> {
+    let http_client = reqwest::Client::new();
+    let request_url = format!("{base}/{index}", base = self.uri, index = index);
+
+    let data = http_client
+    .put(request_url)
+    .header("content-type", "application/json")
+    .header("accept", "application/json")
+    .send()
+    .await
+    .unwrap();
+
+    match data.status() {
+      reqwest::StatusCode::OK => Ok("Index created".to_string()),
+      reqwest::StatusCode::NOT_FOUND => Err("Index not created".to_string()),
+      _ => {
+        panic!("Unexepcted error")
+      }
+    }
+  }
+
+  async fn create_index_if_not_exists(self, index: String) -> bool {
     let http_client = reqwest::Client::new();
     let request_url = format!("{base}/{index}", base = self.uri, index = index);
 
@@ -29,15 +52,19 @@ impl Elasticsearch {
 
     match data.status() {
       reqwest::StatusCode::OK => return true,
-      reqwest::StatusCode::NOT_FOUND => return false,
+      reqwest::StatusCode::NOT_FOUND => {
+        self.create_index(index).await;
+
+        return true
+      },
       _ => {
         panic!("Statuscode don't match 200 or 404")
       }
     }
   }
 
-  pub async fn bulk_add() {
-
+  pub async fn bulk_add(self, index: String, data: HashMap<String, String>) -> bool {
+    self.create_index_if_not_exists(index).await
   }
 }
 
@@ -48,3 +75,4 @@ impl Default for Elasticsearch {
     }
   }
 }
+ 
